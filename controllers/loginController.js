e67c9d05c24561c45147457f32de2f3d8d8b5df1
Antenor <antenorzapata@gmail.com>
2021-08-30 23:27:20 -0300

    refactor: add validation middlware in the login route

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,16 +1,6 @@
 const generateToken = require('../utils/crypto');
-const VerifyLoginCredentials = require('../utils/verifyCredentials');
 
-const login = (req, res) => {
-  const verify = new VerifyLoginCredentials(req, res);
-  const vEamil = verify.verifyEmail();
-  const vPassword = verify.verifyPassword();
-  if (vEamil && vPassword) {
-    return res.status(200).json({
-      token: generateToken(),
-    });
-  }
-};
+const login = (req, res) => res.status(200).json({ token: generateToken() });
 
 module.exports = {
   login,
